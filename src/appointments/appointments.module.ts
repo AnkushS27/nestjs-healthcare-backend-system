7b4bbe262854d5858c6bc4a40c1fd517e83dea70
AnkushS27 <ankush27singh11@gmail.com>
2025-03-04 16:57:11 +0530

    feat: integrate abac to appointments controller

--- src/appointments/appointments.module.ts
+++ src/appointments/appointments.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppointmentsService } from './appointments.service';
 import { AppointmentsController } from './appointments.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { AccessPoliciesModule } from 'src/access-policies/access-policies.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AccessPoliciesModule],
   providers: [AppointmentsService],
   controllers: [AppointmentsController]
 })
